4dcafe98d8d2980c9949351cbf9e28b82ff4f1ca
Manish Gond <manish.n.gond@gmail.com>
2025-08-19 21:53:44 +0530

    Updated code

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  target: 'node',
-  entry: './src/extension.ts',
+  mode: "production",
+  target: "node",
+  entry: "./src/extension.ts",
   output: {
-    path: path.resolve(__dirname, 'out'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-    clean: true
+    path: path.resolve(__dirname, "out"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+    clean: true,
   },
   externals: {
-    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    // modules added here also need to be added in the .vscodeignore file
-    typescript: 'commonjs typescript'
+    vscode: "commonjs vscode",
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: [".ts", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
+        use: "ts-loader",
+      },
+    ],
   },
-  devtool: 'nosources-source-map'
+  devtool: "nosources-source-map",
+  optimization: {
+    minimize: true,
+    usedExports: true,
+  },
 };
